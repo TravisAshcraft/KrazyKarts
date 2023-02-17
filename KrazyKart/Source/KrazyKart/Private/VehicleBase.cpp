bc38ad36913c1c1347e9586558427e7339bf9c4f
TravisAshcraft <58603687+TravisAshcraft@users.noreply.github.com>
2023-02-17 17:18:19 -0600

    Replicating movement working on client side prediction

--- KrazyKart/Source/KrazyKart/Private/VehicleBase.cpp
+++ KrazyKart/Source/KrazyKart/Private/VehicleBase.cpp
@@ -2,6 +2,8 @@
 
 
 #include "VehicleBase.h"
+#include "DrawDebugHelpers.h"
+#include "Net/UnrealNetwork.h"
 
 // Sets default values
 AVehicleBase::AVehicleBase()
@@ -9,18 +11,53 @@ AVehicleBase::AVehicleBase()
  	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	bReplicates = true;
+
 }
 
 // Called when the game starts or when spawned
 void AVehicleBase::BeginPlay()
 {
 	Super::BeginPlay();
+
+	if(HasAuthority())
+	{
+		NetUpdateFrequency = 1;
+	}
 	
 }
 
-
-void AVehicleBase::UpdateLocationFromVelocity(FVector Delta)
+void AVehicleBase::GetLifetimeReplicatedProps(TArray<FLifetimeProperty> & OutLifetimeProps) const
 {
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+	DOREPLIFETIME(AVehicleBase, ReplicatedLocation);
+	DOREPLIFETIME(AVehicleBase, ReplcatedRotation);
+	DOREPLIFETIME(AVehicleBase, Velocity);
+	DOREPLIFETIME(AVehicleBase, Throttle);
+	DOREPLIFETIME(AVehicleBase, SteeringThrow);
+}
+
+FString GetEnumText(ENetRole Role)
+{
+	switch (Role)
+	{
+	case ROLE_None:
+		return "None";
+	case ROLE_Authority:
+		return "Authority";
+	case ROLE_AutonomousProxy:
+		return "Autonomous Proxy";
+	case ROLE_SimulatedProxy:
+		return "Simulated Proxy";
+	default:
+		return "ERROR";
+	}
+}
+
+
+void AVehicleBase::UpdateLocationFromVelocity(float DeltaTime)
+{
+	FVector Delta = Velocity * 100 * DeltaTime;
 	FHitResult HitResult;
 	
 	AddActorWorldOffset(Delta, true, &HitResult);
@@ -33,31 +70,77 @@ void AVehicleBase::UpdateLocationFromVelocity(FVector Delta)
 
 void AVehicleBase::ApplyRotation(float DeltaTime)
 {
-	float RotationAngle = MaxDegreePersecond * DeltaTime * SteeringThrow;
-
-	FQuat RotationDelta(GetActorUpVector(), FMath::DegreesToRadians(RotationAngle));
+	float DeltaLocation = FVector::DotProduct(GetActorForwardVector(), Velocity) * DeltaTime;
+	float RotationAngle = DeltaLocation / MinTurningRadius * SteeringThrow;
+	FQuat RotationDelta(GetActorUpVector(), RotationAngle);
 
 	Velocity = RotationDelta.RotateVector(Velocity);
 
 	AddActorWorldRotation(RotationDelta);
+
+	UpdateLocationFromVelocity(DeltaTime);
 }
+
+FVector AVehicleBase::GetDragResistance()
+{
+	return -Velocity.GetSafeNormal() * Velocity.SizeSquared() * DragCoefficient;
+}
+
+FVector AVehicleBase::GetRollingResistance()
+{
+	
+	//Noraml Force is the gravity being applied in the zed axis downward
+	float AccelerationDueToGravity = -GetWorld()->GetGravityZ() / 100;
+	float NormalForce = Mass * AccelerationDueToGravity;
+	return -Velocity.GetSafeNormal() * RollingResistance * NormalForce;
+}
+
+
+
 
 // Called every frame
 void AVehicleBase::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
-
+	
+	//Add air resistance to force it will eventually zero out
+	
 	FVector Force = GetActorForwardVector() * MaxDrivingForce * Throttle;
+	Force += GetDragResistance();
+	Force += GetRollingResistance();
+	
 	FVector Acceleration = Force / Mass;
-
+	
+	// Rolling Resistance = RRCoefficient X NormalForce
 	Velocity = Velocity + Acceleration * DeltaTime;
 
-	FVector Delta = Velocity * 100 * DeltaTime;
-
-	UpdateLocationFromVelocity(Delta);
+	UpdateLocationFromVelocity(DeltaTime);
 
 	ApplyRotation(DeltaTime);
+
+	if(HasAuthority())
+	{
+		ReplicatedLocation = GetActorLocation();
+		ReplcatedRotation = GetActorRotation();
+	}
+
+	DrawDebugString(GetWorld(), FVector(0,0, 100), GetEnumText(GetLocalRole()), this, FColor::White, DeltaTime)
+
+	// 1. Create a new Move
+	// 2. Save a list of unacknowledged moves
+	// 3. Send the move to the server
+	// 4. Simulate the move locally
 }
+
+// void AVehicleBase::OnRecieveMove()
+// 1. Check that the move is valid (No Cheating)
+// 2. Simulate the move
+// 3. Send the cannonical State to the clients
+
+// void OnRecieveServerState
+// 1. Remove all moves incleded in state
+// 2. Reset to server state
+// 3. Replay/Simulate unacknowledged moves
 
 // Called to bind functionality to input
 void AVehicleBase::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
@@ -68,13 +151,46 @@ void AVehicleBase::SetupPlayerInputComponent(UInputComponent* PlayerInputCompone
 	PlayerInputComponent->BindAxis("MoveRight", this, &AVehicleBase::MoveRight);
 }
 
+void AVehicleBase::OnRep_ReplicatedLocation()
+{
+	UE_LOG(LogTemp, Warning, TEXT("Replicated Location"));
+
+	SetActorLocation(ReplicatedLocation);
+	SetActorRotation(ReplcatedRotation);
+
+}
+
 void AVehicleBase::MoveForward(float Value)
 {
 	Throttle = Value;
+	Server_MoveForward(Value);
 }
 
 void AVehicleBase::MoveRight(float Value)
 {
 	SteeringThrow = Value;
+	Server_MoveRight(Value);
 }
+
+void AVehicleBase::Server_MoveForward_Implementation(float Value)
+{
+	Throttle = Value;
+}
+
+bool AVehicleBase::Server_MoveForward_Validate(float Value)
+{
+	return FMath::Abs(Value) <= 1;
+}
+
+
+void AVehicleBase::Server_MoveRight_Implementation(float Value)
+{
+	SteeringThrow = Value;
+}
+
+bool AVehicleBase::Server_MoveRight_Validate(float Value)
+{
+	return FMath::Abs(Value) <= 1;
+}
+
 
--- KrazyKart/Source/KrazyKart/Public/VehicleBase.h
+++ KrazyKart/Source/KrazyKart/Public/VehicleBase.h
@@ -6,6 +6,40 @@
 #include "GameFramework/Pawn.h"
 #include "VehicleBase.generated.h"
 
+USTRUCT()
+struct FVehicleMove
+{
+	GENERATED_BODY()
+
+	UPROPERTY()
+	float Throttle;
+	UPROPERTY()
+	float SteeringThrow;
+
+	UPROPERTY()
+	float DeltaTime;
+	
+};
+
+USTRUCT()
+struct FVehicleState
+{
+	GENERATED_BODY()
+
+	UPROPERTY()
+	FVehicleMove LastMove;
+
+	UPROPERTY()
+	FVector Velocity;
+
+	UPROPERTY()
+	FVector Location;
+
+	UPROPERTY()
+	FRotator Rotation;
+	
+};
+
 UCLASS()
 class KRAZYKART_API AVehicleBase : public APawn
 {
@@ -18,9 +52,10 @@ public:
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
-	void UpdateLocationFromVelocity(FVector Delta);
+	void UpdateLocationFromVelocity(float DeltaTime);
 	void ApplyRotation(float DeltaTime);
-
+	FVector GetDragResistance();
+	FVector GetRollingResistance();
 public:	
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
@@ -36,18 +71,45 @@ private:
 	UPROPERTY(EditAnywhere)
 	float MaxDrivingForce = 10000;
 
-	// The number of degrees rotated per second
+	// The min Turn radius at full lock (m).
 	UPROPERTY(EditAnywhere)
-	float MaxDegreePersecond = 90;
+	float MinTurningRadius = 10;
 
+	// The amount of drag on this vehicle
+	// Higher the number the more resistance.
+	UPROPERTY(EditAnywhere)
+	float DragCoefficient = 16;
+
+	// The amount of Rolling Resistance on this vehicle
+	// Higher the number the more resistance.
+	UPROPERTY(EditAnywhere)
+	float RollingResistance = 0.015;
+
+	UPROPERTY(Replicated)
 	float Throttle;
+	UPROPERTY(Replicated)
 	float SteeringThrow;
 
+	UPROPERTY(Replicated)
 	FVector Velocity;
 
-	
+	UPROPERTY(ReplicatedUsing= OnRep_ReplicatedLocation)
+	FVector ReplicatedLocation;
+
+	//OnReps have to be flagged as UFunction()
+	UFUNCTION() 
+	void OnRep_ReplicatedLocation();
+
+	UPROPERTY(Replicated)
+	FRotator ReplcatedRotation;
 
 	void MoveForward(float Value);
 	void MoveRight(float Value);
+	
+	UFUNCTION(Server, Reliable, WithValidation)
+	void Server_MoveForward(float Value);
+
+	UFUNCTION(Server, Reliable, WithValidation)
+	void Server_MoveRight(float Value);
 
 };
